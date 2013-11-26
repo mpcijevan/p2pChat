a33dedc9315aaab3b59318de69024da4307e3f32
Serine <l@l-Satellite-C660.(none)>
2013-11-26 21:51:43 +0400

    @Brief:full commit

---
+++ p2pchat/src/com/mpci/p2pchat/UserProfile.java
@@ -0,0 +1,11 @@
+package com.mpci.p2pchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UserProfile extends Activity{
+	  protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.user_profile);
+	  }
+}
